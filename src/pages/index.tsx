c772ad34a7447dc464d3a5670d83bd63416f8576
Moto1224 <s1F102401809@iniad.org>
2024-05-23 02:49:33 +0900

    あいう

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,39 +12,24 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [turnColor, setTurnColor] = useState(1);
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
-    const newBoard = structuredClone(board);
-    const maybeTurnables: { x: number; y: number }[] = [];
-
-    for (let n: number = 1; n < 8; n++) {
-      if (board[y + n][x] === 3 - turnColor) {
-        maybeTurnables.push({ x, y: y + n });
-      }
-      else if (board[y + n][x] === turnColor) {
-        for (const turnable of maybeTurnables) {
-          newBoard turnable = turnColor;
-        }
+    const newBoard = JSON.parse(JSON.stringify(board));
+    if (newBoard[y][x] === 0) {
+      if (newBoard[y + 1] !== undefined && newBoard[y + 1][x] === 3 - turnColor) {
+        newBoard[y][x] = turnColor;
+        setBoard(newBoard);
+        setTurnColor(3 - turnColor);
       }
     }
-    if (board[y + 4][x] === turnColor) {
-      if (board[y + 1][x] === 3 - turnColor) newBoard[y][x] = turnColor;
-      newBoard[y + 1][x] = turnColor;
-      setTurnColor(3 - turnColor);
-
-      if (board[y + 2][x] === 3 - turnColor) newBoard[y + 2][x] = turnColor;
-
-      if (board[y + 3][x] === 3 - turnColor) newBoard[y + 3][x] = turnColor;
-    }
-    setBoard(newBoard);
   };
-
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+            <div className={styles.cell} key={`${x * 20}-${y}`} onClick={() => clickHandler(x, y)}>
               {color !== 0 && (
                 <div
                   className={styles.stone}
